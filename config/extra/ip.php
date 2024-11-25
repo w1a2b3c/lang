32028f52c4a1b7c56425d3ddc504328fb6ee7e0e
Eric <127228647+w1a2b3c@users.noreply.github.com>
2024-11-25 22:04:47 +0800

    Add files via upload

---
+++ config/extra/ip.php
@@ -0,0 +1,5 @@
+<?php
+return [
+	'onoff' => true, //表示关闭
+    'ip' => "xychemkj.com",
+];
---
+++ config/extra/web.php
@@ -0,0 +1,11 @@
+<?php
+return [
+    'web' => array(
+		'title' => '付费进群系统',
+		'oaname' => '付费进群系统',
+		'softname' => '付费进群系统',
+		'version' => '3.5.8',
+		'copyright' => '芒果资源网'
+	),
+	'page' => 20,
+];
